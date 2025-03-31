eade11321f161efa3120fa7660135edd93a141ff
Anastasia Mousa <anastasiamousa34@gmail.com>
2025-03-31 12:46:04 +0300

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Anastasia Mousa on 31/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
